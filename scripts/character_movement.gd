e638923643605e7ef337cf0e466f451fa09b66a6
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-04-14 18:53:40 +0800

    ci: rewrite character movement script to be more readable

--- scripts/character_movement.gd
+++ scripts/character_movement.gd
@@ -13,8 +13,15 @@ var ground_timer: float;
 @export var input_direction: InputDirection = null;
 var gravity_multiplier: float = 1;
 
+var left_direction: Vector2: get = get_left;
+
+var instant_acceleration: Vector2 = Vector2.ZERO;
+
 func jump():
 	self.jump_timer = self.leading_jump_buffer;
+
+func add_instant_acceleration(acceleration: Vector2) -> void:
+	self.instant_acceleration += acceleration;
 
 func _physics_process(delta: float):
 	if self.jump_timer > 0.:
@@ -26,41 +33,55 @@ func _physics_process(delta: float):
 	if self.is_on_floor():
 		self.ground_timer = self.trailing_jump_buffer;
 	
-	var up = self.get_up_direction();
-	var left = Vector2(up.y, -up.x);
 	var temp_velocity = self.get_velocity();
 	
 	var delta_v = Vector2(0., 0.);
 	var x_axis = self.input_direction.get_direction().x;
 	if x_axis != 0:
-		var distance_from_max_v = self.max_walk_speed * abs(x_axis) + left.dot(temp_velocity) * sign(x_axis);
-		if self.is_on_floor() && (left.dot(temp_velocity) * sign(x_axis) > 0.):
-			delta_v += -temp_velocity.project(left) * self.reverse_boost;
-		
-		if distance_from_max_v > 0.:
-			if distance_from_max_v < self.horizontal_accel_speed * delta :
-				delta_v += -sign(x_axis) * left * (distance_from_max_v / delta);
-			else:
-				delta_v += -sign(x_axis) * self.horizontal_accel_speed * left;
-	else:
-		if self.is_on_floor():
-			delta_v += -temp_velocity.project(left) * self.reverse_boost;
+		delta_v += self.get_walk_force(temp_velocity, x_axis, delta);
+	
+	delta_v += self.get_reverse_boost(temp_velocity,x_axis);
+	
 	delta_v += self.get_gravity() * self.gravity_multiplier;
 	delta_v += -temp_velocity * self.air_friction;
 
-	var delta_v_instant = Vector2(0., 0.);
-
 	if (self.jump_timer > 0.) && (self.ground_timer > 0.):
-		self.jump_timer = 0.;
-		self.ground_timer = 0.;
-		delta_v_instant += up * self.jump_y_vel;
-		delta_v_instant += -temp_velocity.project(up);
+		self.apply_jump(temp_velocity);
 	
-	self.set_velocity(temp_velocity + delta_v * delta + delta_v_instant);
+	self.set_velocity(temp_velocity + delta_v * delta + self.instant_acceleration);
 	self.move_and_slide();
+	self.instant_acceleration = Vector2.ZERO;
+
+func get_walk_force(temp_velocity: Vector2, x_input: float, delta: float) -> Vector2:
+	var delta_v: Vector2 = Vector2.ZERO;
+	var distance_from_max_v = self.max_walk_speed * abs(x_input) 
+	distance_from_max_v += self.left_direction.dot(temp_velocity) * sign(x_input);
+	
+	if distance_from_max_v > 0.:
+		if distance_from_max_v < self.horizontal_accel_speed * delta :
+			delta_v += -sign(x_input) * (distance_from_max_v / delta) * self.left_direction;
+		else:
+			delta_v += -sign(x_input) * self.horizontal_accel_speed * self.left_direction;
+	return delta_v;
+
+func get_reverse_boost(temp_velocity: Vector2, x_input: float) -> Vector2:
+	if x_input == 0 || self.left_direction.dot(temp_velocity) * sign(x_input) > 0.:
+		return -temp_velocity.project(self.left_direction) * self.reverse_boost;
+	return Vector2.ZERO;
+
+func apply_jump(temp_velocity: Vector2):
+	self.jump_timer = 0.;
+	self.ground_timer = 0.;
+	self.add_instant_acceleration(
+		up_direction * self.jump_y_vel 
+		- temp_velocity.project(up_direction)
+	);
 
 func set_gravity_multiplier(multiplier: float):
 	self.gravity_multiplier = multiplier;
 
 func reset_gravity_multiplier():
 	self.gravity_multiplier = 1;
+
+func get_left() -> Vector2:
+	return Vector2(self.up_direction.y, -self.up_direction.x);
